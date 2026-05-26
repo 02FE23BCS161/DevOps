fbab24b7634e031fce54259d3d962d3a5e6270c4
tanvi <161680432+TanviShikhari@users.noreply.github.com>
2026-05-26 09:33:05 +0530

    Fixed route

--- Lost and Found/lost-and-found/backend/routes/notificationRoutes.js
+++ Lost and Found/lost-and-found/backend/routes/notificationRoutes.js
@@ -1,11 +1,13 @@
-const express = require('express');
+05const express = require('express');
 const router = express.Router();
 const { getNotifications, markAsRead } = require('../controllers/notificationController');
 const { protect } = require('../middlewares/authMiddleware');
 
+// Root routes 
 router.route('/')
   .get(protect, getNotifications);
 
+// Read route
 router.route('/:id/read')
   .put(protect, markAsRead);
 
